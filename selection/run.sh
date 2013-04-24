05da1b9883ab69503f3fdaf8f40ae5149aba1c4a
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2013-04-24 13:38:12 -0400

    Scripts for selection data time series

---
+++ selection/run.sh
@@ -0,0 +1,2 @@
+python sql.py > query.sql
+python extract.py > output/selection_timeseries_fy13.csv 
---
+++ selection/settings.example.py
@@ -0,0 +1,8 @@
+
+settings={
+        "pswd":"your password here",#base64.b64decode('not-really-secure-either'),
+        "host":"172.21.4.82\clusql02",
+        "user":"MCC\yourusername",
+        "database":"DPESel_Prod13"
+}
+                     
