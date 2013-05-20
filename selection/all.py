def096c5ada9a79ec88dbbf12b5601d41f0bb7ba
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2013-05-20 02:10:22 -0400

    Added "static" datasets/files

--- selection/all.py
+++ selection/all.py
@@ -5,6 +5,7 @@ import timeseries_export
 
 from settings import settings
 import os
+import shutil
 
 if not os.path.exists("output"): os.makedirs("output")
 
@@ -25,5 +26,14 @@ for fy in settings:
 		print "Exporting timeseries data from database"
 		timeseries_export.run(settings[fy], fy)
 
+
+	print "Copying all static datafiles: ", fy
+
+	src="config/"+fy+"/static/"
+	src_files = os.listdir(src)
+	for file_name in src_files:
+	    	full_file_name = os.path.join(src, file_name)
+	    	if (os.path.isfile(full_file_name)):
+        		shutil.copy(full_file_name, "output/"+fy+"/")
 
 
